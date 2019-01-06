49e97db3addc376411f1110f022522157f31ec0e
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2019-01-06 14:36:47 -0330

    Publish GH pages branch

---
+++ precache-manifest.b1f7f584ebe0d046258a6622732d23d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "78f75de97bcd4c14f3ce",
+    "url": "/todo.app/static/js/runtime~main.78f75de9.js"
+  },
+  {
+    "revision": "17db7c93f1120e78fa06",
+    "url": "/todo.app/static/js/main.17db7c93.chunk.js"
+  },
+  {
+    "revision": "c4e05109685a38388424",
+    "url": "/todo.app/static/js/1.c4e05109.chunk.js"
+  },
+  {
+    "revision": "17db7c93f1120e78fa06",
+    "url": "/todo.app/static/css/main.c7498248.chunk.css"
+  },
+  {
+    "revision": "c4e05109685a38388424",
+    "url": "/todo.app/static/css/1.724d2934.chunk.css"
+  },
+  {
+    "revision": "df366b28563876c1085ae6a8f845779c",
+    "url": "/todo.app/index.html"
+  }
+];
