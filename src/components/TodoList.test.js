666f2e43ef75e270caf9fed22574b74d72edb703
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2019-01-06 14:41:12 -0330

    Delete TodoList test
    👀

--- src/components/TodoList.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {TodoList} from './TodoList';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<TodoList items={[]} />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
